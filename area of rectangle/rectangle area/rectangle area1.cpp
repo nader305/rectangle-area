a90843f3fb6c1b51634518e1ecf700c54325c37c
nader305 <engnaderwahbaa@gmail.com>
2024-07-30 09:58:42 +0300

    Add files via upload

---
+++ area of rectangle/rectangle area/rectangle area1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int w, l, area;
+	cout << "Enter a Width of rectangle:\n";
+	cin >> w;
+	cout << "Enter a Length of rectangle:\n";
+	cin >> l;
+	cout << "===============\n";
+	area = w * l;
+	cout << "The area is :" << area << endl;
+	return 0;
+
+}
---
+++ area of rectangle/rectangle area/rectangle area2.cpp
@@ -0,0 +1,17 @@
+//#include<iostream>
+//using namespace std;
+//int getArea(int w, int l) {
+//	int area = w * l;
+//	return area;
+//}
+//int main() {
+//		int w, l, area;
+//		cout << "Enter a Width of rectangle:\n";
+//		cin >> w;
+//		cout << "Enter a Length of rectangle:\n";
+//		cin >> l;
+//		cout << "===============\n";
+//		area = getArea(w,l);
+//		cout << "The area is :" << area << endl;
+//		return 0;
+//}
